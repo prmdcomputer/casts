636feee219182290807e840e658599c253f15bde
prmdcomputer <prmdcomputer@gmail.com>
2026-02-03 10:55:03 +0530

    Update certificate-preview.tsx

--- src/components/certificate-preview.tsx
+++ src/components/certificate-preview.tsx
@@ -48,9 +48,9 @@ export function CertificatePreview() {
         {watermark && <Image 
             src={watermark.imageUrl} 
             alt={watermark.description}
-            width={500}
-            height={500}
-            style={{position: 'absolute', top: '45%', left: '45%', transform: 'translate(-50%, -50%)', opacity: 0.4, pointerEvents: 'none'}} 
+            width={450}
+            height={450}
+            style={{position: 'absolute', top: '50%', left: '45%', transform: 'translate(-50%, -50%)', opacity: 0.4, pointerEvents: 'none'}} 
             data-ai-hint={watermark.imageHint}
         />}
 
